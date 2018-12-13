0697788c2d4a2f8cda30482a5bfe97f4ee49e657
Wouter <wouter075@gmail.com>
2018-12-13 10:38:33 +0100

    Refactor __init__

--- magister.py
+++ magister.py
@@ -59,67 +59,62 @@ class Magister:
         # get authorizeUrl
         r = self.__s.get(self.authorizeUrl, allow_redirects=False)
 
-        if r.status_code == 302:
-            self.__returnUrl = unquote(r.headers['Location'].split("returnUrl=")[1])
-
-            r2 = self.__s.get(r.headers['Location'], allow_redirects=False)
-            if r2.status_code == 302:
-                self.__sessionId = r2.headers['Location'] \
-                    .split("?")[1] \
-                    .split("&")[0] \
-                    .split("=")[1]
-
-                # get xsrf
-                self.__xsrf = r2.headers['Set-Cookie'].split("XSRF-TOKEN=")[1].split(";")[0]
-
-                # prepare data
-                data = {
-                    "sessionId": self.__sessionId,
-                    "returnUrl": self.__returnUrl,
-                    "username": self.username
-                }
-
-                # prapare headers
-                headers = {
-                    "X-XSRF-TOKEN": self.__xsrf
-                }
-
-                # post username
-                r3 = self.__s.post(self.__authUrl + "username", json=data, headers=headers)
-                if r3.status_code == 200:
-                    # post password
-                    data = {
-                        "sessionId": self.__sessionId,
-                        "returnUrl": self.__returnUrl,
-                        "password": self.password
-                    }
-                    r4 = self.__s.post(self.__authUrl + "password", json=data, headers=headers)
-
-                    if r4.status_code == 200:
-                        r5 = self.__s.get(self.__accountUrl + self.__returnUrl, headers=headers, allow_redirects=False)
-                        if r5.status_code == 302:
-                            self.__bearerToken = r5.headers["Location"].split("&access_token=")[1].split("&")[0]
-
-                            # set headers to use everywhere!
-                            self.__headers = {
-                                "Authorization": "Bearer " + self.__bearerToken,
-                                "X-XSRF-TOKEN": self.__xsrf
-                            }
-
-                        else:
-                            raise RuntimeError("BearerToken error " + str(r5.status_code))
-
-                    else:
-                        raise RuntimeError("Password error " + str(r4.status_code))
-
-                else:
-                    raise RuntimeError("Username error " + str(r3.status_code))
-
-            else:
-                raise RuntimeError("SessionId error " + str(r2.status_code))
-
-        else:
+        if r.status_code != 302:
             raise RuntimeError("ReturnUrl error " + str(r.status_code))
+
+        self.__returnUrl = unquote(r.headers['Location'].split("returnUrl=")[1])
+
+        r2 = self.__s.get(r.headers['Location'], allow_redirects=False)
+        if r2.status_code != 302:
+            raise RuntimeError("SessionId error " + str(r2.status_code))
+
+        self.__sessionId = r2.headers['Location'] \
+            .split("?")[1] \
+            .split("&")[0] \
+            .split("=")[1]
+
+        # get xsrf
+        self.__xsrf = r2.headers['Set-Cookie'].split("XSRF-TOKEN=")[1].split(";")[0]
+
+        # prepare data
+        data = {
+            "sessionId": self.__sessionId,
+            "returnUrl": self.__returnUrl,
+            "username": self.username
+        }
+
+        # prapare headers
+        headers = {
+            "X-XSRF-TOKEN": self.__xsrf
+        }
+
+        # post username
+        r3 = self.__s.post(self.__authUrl + "username", json=data, headers=headers)
+        if r3.status_code != 200:
+            raise RuntimeError("Username error " + str(r3.status_code))
+
+        # post password
+        data = {
+            "sessionId": self.__sessionId,
+            "returnUrl": self.__returnUrl,
+            "password": self.password
+        }
+
+        r4 = self.__s.post(self.__authUrl + "password", json=data, headers=headers)
+        if r4.status_code != 200:
+            raise RuntimeError("Password error " + str(r4.status_code))
+
+        r5 = self.__s.get(self.__accountUrl + self.__returnUrl, headers=headers, allow_redirects=False)
+        if r5.status_code != 302:
+            raise RuntimeError("BearerToken error " + str(r5.status_code))
+
+        self.__bearerToken = r5.headers["Location"].split("&access_token=")[1].split("&")[0]
+
+        # set headers to use everywhere!
+        self.__headers = {
+            "Authorization": "Bearer " + self.__bearerToken,
+            "X-XSRF-TOKEN": self.__xsrf
+        }
 
     def profileinfo(self):
         """ this function retrieves the users profile. """
@@ -191,5 +186,5 @@ class Magister:
 
 
 m = Magister("https://novacollege.magister.net", "hlw1404", getpass("Password: "))
-print(m.setstudentreason(185304, 12142666, Reason.Aanwezig))
+print(m.profileinfo())
 
